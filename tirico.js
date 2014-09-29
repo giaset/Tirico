812abaf635b894414b5c1e28fae9cf352e463ff9
Gianni Settino <g@miltonandparc.com>
2014-09-29 00:04:00 -0400

    Use game_id from command line arguments. Rename some variables.

--- tirico.js
+++ tirico.js
@@ -1,21 +1,21 @@
 var request = require('request')
 
-var game_id = '2014092810'
+var game_id = process.argv[2]
 
-var last_play = 0
+var last_play_id = 0
 
 function get_plays() {
 	request('http://www.nfl.com/liveupdate/game-center/'+game_id+'/'+game_id+'_gtd.json', function (error, response, body) {
 		if (!error && response.statusCode == 200) {
 			var game = JSON.parse(body)
 			var drives = game[game_id].drives
-			for (drive in drives) {
-				var plays = drives[drive].plays
+			for (drive_id in drives) {
+				var plays = drives[drive_id].plays
 				for (play in plays) {
-					var play_num = parseInt(play)
-					if (play_num > last_play) {
-						last_play = play_num
-						console.log(plays[play_num].desc)
+					var play_id = parseInt(play)
+					if (play_id > last_play_id) {
+						last_play_id = play_id
+						console.log(plays[play_id].desc)
 					}
 				}
 			}
